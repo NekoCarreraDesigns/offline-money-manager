c0a98162c1701d251c6027361498e2b557f4201c
NekoCarreraDesigns <denvermaas@gmail.com>
2020-07-30 00:22:17 -0600

    changed the routes to controllers in server.js

--- server.js
+++ server.js
@@ -17,11 +17,12 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./controllers/api.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
